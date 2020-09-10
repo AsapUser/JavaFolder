9dd4ecd5cb54c1057fadb33b3159ecfd85c44be9
AsapUser <denis133713372@gmail.com>
2020-09-10 01:50:48 -0400

    New changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,17 @@ package com.company;
 
 public class Main {
 
+
+
     public static void main(String[] args) {
-	// write your code here
+
+        short s = 2222;
+        int i = 6467886;
+        long l = 211111111L;
+        System.out.println(i);
+        System.out.println(s);
+        System.out.println(l);
+        System.out.println("Hello");
+
     }
 }
