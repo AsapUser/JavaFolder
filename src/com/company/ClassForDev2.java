69644cadcab49b07f5bea76e2acd1876efa50dba
AsapUser <denis133713372@gmail.com>
2020-09-16 08:02:47 -0500

    added class for dev2

---
+++ src/com/company/ClassForDev2.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class ClassForDev2 {
+
+    public static void main(String[] args) {
+
+           int m = 76;
+           int k = 10;
+
+           int y = m-k;
+
+        System.out.println(y + " dev2 ");
+
+
+    }
+}
