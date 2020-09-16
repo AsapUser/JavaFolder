105a3cd51c25303cef7d53f7d83cef1b769574c5
AsapUser <denis133713372@gmail.com>
2020-09-16 07:58:03 -0500

    added new class for dev1

---
+++ src/com/company/ClassForDev1.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class ClassForDev1 {
+    public static void main(String[] args) {
+
+
+        short s = 2222;
+        int i = 6467886;
+        long l = 211111111L;
+        System.out.println(i);
+        System.out.println(s);
+        System.out.println(l);
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello dev1");
+
+
+    }
+}
