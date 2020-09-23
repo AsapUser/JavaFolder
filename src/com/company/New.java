cb84fd5a9dd901e4d82f87e55d779b3b40a6cec5
AsapUser <denis133713372@gmail.com>
2020-09-23 13:29:24 -0400

    Delete hi master

--- src/com/company/New.java
+++ src/com/company/New.java
@@ -3,9 +3,7 @@ package com.company;
 public class New {
     public static void main(String[] args) {
 
-        System.out.println("Hi master");
         System.out.println("hi dev");
-
-
+        
     }
 }
